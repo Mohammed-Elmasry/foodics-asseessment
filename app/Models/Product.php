652a5ac40d2718a36a5ce48447f252e2bf918fcf
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-07 12:12:09 +0200

    define relation with ingredients

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class)->withPivot('quantity')->withTimestamps();
     }
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class)->withPivot('used_amount')->withTimestamps();
+    }
 }

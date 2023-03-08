87136f933df3f90c795cbaa2585bcd91aae2b823
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 14:00:01 +0200

    Utilize IngredientsService within OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderCreationRequest;
 use App\Models\Order;
+use App\Models\Product;
+use App\Services\IngredientsService;
 use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
@@ -14,9 +16,11 @@ class OrderController extends Controller
         $order = Order::create();
 
         $products = $request->input("products");
+        $service = new IngredientsService();
 
         foreach ($products as $product) {
             $order->products()->attach($product["product_id"], ["quantity" => $product["quantity"]]);
+            $service->updateIngredientsAmounts(Product::find($product["product_id"]), $product["quantity"]);
         }
 
         return response()->json(["message" => "Order Created"], Response::HTTP_CREATED);

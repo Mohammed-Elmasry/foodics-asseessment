dd383945544e0a1cccd6223861a2ffab330cdd53
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 14:24:24 +0200

    add method availableAmountInKilos()

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Constants;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -18,5 +19,10 @@ class Ingredient extends Model
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class)->withPivot('used_amount')->withTimestamps();
+    }
+
+    public function availableAmountInKilos()
+    {
+        return $this->available_amount_in_grams / Constants::KILO;
     }
 }

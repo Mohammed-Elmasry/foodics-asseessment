fa4060490181825ca28e609e24b75a2e19f99b34
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 20:03:15 +0200

    add original_total_amount field

--- database/migrations/2023_03_07_084424_create_ingredients_table.php
+++ database/migrations/2023_03_07_084424_create_ingredients_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->integer("available_amount_in_grams");
+            $table->integer("original_total_amount");
             $table->timestamps();
         });
     }

ca8ec3aa092e540630ebd6ec433fbfb3f8f680b8
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 20:04:01 +0200

    add notification logic within IngredientsService

--- app/Services/IngredientsService.php
+++ app/Services/IngredientsService.php
@@ -4,12 +4,15 @@
 namespace App\Services;
 
 
+use App\Mail\StockDepletionEmail;
 use App\Models\Ingredient;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class IngredientsService
 {
+    const NOTIFICATION_THRESHOLD_RATIO = 0.5;
 
     public function updateIngredientsAmounts(Product $product, int $quantity = 1)
     {
@@ -17,6 +20,12 @@ class IngredientsService
             foreach ($product->ingredients as $ingredient) {
                 $ingredient->available_amount_in_grams -= ($ingredient->pivot->used_amount * $quantity);
                 $ingredient->save();
+
+                if ($ingredient->available_amount_in_grams < $ingredient->notificationThreshold()) {
+                    Mail::mailer("smtp")
+                        ->to("merchant@product.com")
+                        ->send(new StockDepletionEmail($ingredient));
+                }
             }
         });
     }

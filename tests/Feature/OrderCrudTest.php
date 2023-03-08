6944be99b22963def63facbe79880ed612013dff
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 14:00:18 +0200

    define test for Service Controller integration

--- tests/Feature/OrderCrudTest.php
+++ tests/Feature/OrderCrudTest.php
@@ -2,36 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\Models\Ingredient;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class OrderCrudTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * @var string
-     */
+    const KILO = 1000;
+
     private string $orderCreationUrl;
-    /**
-     * @var Collection|\Illuminate\Database\Eloquent\Factories\Model|Model
-     */
-    private $product;
+
+    private Product $product;
 
     public function testCreatingOrdersReturnsAJson()
     {
-        $response = $this->sendRequest();
+        $response = $this->post($this->orderCreationUrl, $this->requestData());
 
         $response->assertHeader("Content-Type", "application/json");
     }
 
     public function testCreatingOrdersReturns201Created()
     {
-        $response = $this->sendRequest();
+        $response = $this->post($this->orderCreationUrl, $this->requestData());
 
         $response->assertCreated();
         $response->assertJson(["message" => "Order Created"]);
@@ -39,14 +39,35 @@ class OrderCrudTest extends TestCase
 
     public function testCreatedOrderAddsRecordsToDatabase()
     {
-        $this->sendRequest();
+        $this->post($this->orderCreationUrl, $this->requestData());
 
         $this->assertDatabaseCount("orders", 1);
     }
 
-    private function requestData()
+    public function testMakingANewOrderReducesAmountOfAvailableIngredientsOfOrderedProduct()
     {
-        return [
+        $quantity = rand(1, 10);
+        $this->post($this->orderCreationUrl, $this->requestData([
+            "products" => [
+                [
+                    "product_id" => $this->product->id,
+                    "quantity" => $quantity
+                ]
+            ]
+        ]));
+
+        $beef = Ingredient::where(["name" => "beef"])->first();
+        $cheese = Ingredient::where(["name" => "cheese"])->first();
+        $onion = Ingredient::where(["name" => "onion"])->first();
+
+        $this->assertEquals($beef->available_amount_in_grams, (20 * self::KILO - (150 * $quantity)));
+        $this->assertEquals($cheese->available_amount_in_grams, (5 * self::KILO - (30 * $quantity)));
+        $this->assertEquals($onion->available_amount_in_grams, (1 * self::KILO - (20 * $quantity)));
+    }
+
+    private function requestData(?array $data = [])
+    {
+        $default = [
             "products" => [
                 [
                     "product_id" => $this->product->id,
@@ -54,21 +75,15 @@ class OrderCrudTest extends TestCase
                 ]
             ]
         ];
-    }
-
-    /**
-     * @return \Illuminate\Testing\TestResponse
-     */
-    public function sendRequest(): \Illuminate\Testing\TestResponse
-    {
-        return $this->post($this->orderCreationUrl, $this->requestData());
+        return array_merge($default, $data);
     }
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->seed();
         $this->orderCreationUrl = "/api/orders";
-        $this->product = Product::factory()->create(["product_name" => "Burger"]);
+        $this->product = Product::where(["product_name" => "Burger"])->first();
 
     }
 }

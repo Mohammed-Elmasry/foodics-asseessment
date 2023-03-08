8aaf5a4446395faf5d6ba2ad7573c3b1d41d2707
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-08 20:04:09 +0200

    define notification Email

---
+++ app/Mail/StockDepletionEmail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Ingredient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use League\CommonMark\Extension\CommonMark\Renderer\Block\HeadingRenderer;
+
+class StockDepletionEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var Ingredient
+     */
+    public Ingredient $ingredient;
+
+    /**
+     * Create a new message instance.
+     * @param Ingredient $ingredient
+     */
+    public function __construct(Ingredient $ingredient)
+    {
+        $this->ingredient = $ingredient;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Stock Depletion Warning',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'depletion_email',
+        );
+    }
+}
---
+++ resources/views/depletion_email.blade.php
@@ -0,0 +1,3 @@
+<h1>Some Stock is about to run out</h1>
+
+<h1>{{$ingredient->name}}</h1>

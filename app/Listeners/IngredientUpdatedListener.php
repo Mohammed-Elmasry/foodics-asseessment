5f049caf5c972006cc99d15d11b4ccceccf54a8f
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-09 10:10:03 +0200

    remove unused imports

--- app/Listeners/IngredientUpdatedListener.php
+++ app/Listeners/IngredientUpdatedListener.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Events\IngredientUpdatedEvent;
 use App\Mail\StockDepletionEmail;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
 class IngredientUpdatedListener

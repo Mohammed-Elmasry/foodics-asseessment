be8fd65de9626176739547dc07c7c0322487a13d
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-09 13:59:28 +0200

    establish foreign key constraints on ingredient_product table

--- database/migrations/2023_03_07_090118_create_ingredient_product_table.php
+++ database/migrations/2023_03_07_090118_create_ingredient_product_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,6 +15,10 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('ingredient_id');
             $table->integer('used_amount');
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients');
+            
             $table->timestamps();
         });
     }
